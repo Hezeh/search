6f2fbaa422621028a70342729a0a834f27b5541e
Hezekiah <38362531+Hezeh@users.noreply.github.com>
2020-10-15 02:46:02 +0300

    Added Dockerfile

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ if prod:
     user = os.environ.get("ELASTIC_USER")
     secret = os.environ.get("ELASTIC_SECRET")
     host = os.environ.get("ELASTIC_HOST")
-    es = AsyncElasticsearch([f'https://{user}:{secret}@{localhost}:443'])
+    es = AsyncElasticsearch([f'https://{user}:{secret}@{host}:443'])
 else:
     es = AsyncElasticsearch()
     
