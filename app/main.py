1029bbf73e565d88039c040e0c83778ae019bfc6
Hezekiah <38362531+Hezeh@users.noreply.github.com>
2020-12-24 15:59:29 +0300

    Made several changes

--- app/main.py
+++ app/main.py
@@ -1,11 +1,31 @@
 import os
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Header
 from elasticsearch import AsyncElasticsearch
+from typing import Optional
+import requests
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Suggestions(BaseModel):
+    q: str
+    country_code: Optional[str]
+    language_code: Optional[str]
+
+class Search(BaseModel):
+    q: str
+    country_code: Optional[str]
+    lat: Optional[int]
+    lon: Optional[int]
+    country_code: str
+    language_code: str
+
+
 # Whether the environ is dev or prod
-prod = True
+# prod = os.environ.get('ENV')
+prod = False
 if prod:
     user = os.environ.get("ELASTIC_USER")
     secret = os.environ.get("ELASTIC_SECRET")
@@ -13,21 +33,205 @@ if prod:
     es = AsyncElasticsearch([f'https://{user}:{secret}@{host}'])
 else:
     es = AsyncElasticsearch()
-    
+
+
+@app.get('/')
+async def home_page():
+    return {"Message": "Welcome"}
+
+
 @app.get('/ping')
 async def ping():
     resp = await es.ping()
     return resp
 
-@app.get('/search')
-async def index():
-    resp = await es.search(
-        index="stores-index",
-        body={"query":{"match_all": {}}},
-        size=20,
+@app.get('/suggestions')
+async def search_suggestions(q: str, country_code: str, language_code: str):
+    search = await es.search(
+        index=f'items-{country_code}-{language_code}',
+        # index='products',
+        body={
+            "query": {
+                "multi_match": {
+                    "query": f'{q}',
+                    "fields": ["title", "description"]
+                }
+            }
+        },
+        size=5
     )
-    return resp
+    return search
+
+# @app.get('/suggestions')
+# async def search_suggestions(body: Suggestions):
+#     search = await es.search(
+#         # index=f'items-{country_code}-{language_code}',
+#         index='products',
+#         body={
+#             "query": {
+#                 "multi_match": {
+#                     "query": f'{body.q}',
+#                     "fields": ["title", "description"]
+#                 }
+#             }
+#         },
+#         size=5
+#     )
+#     return search
+
+@app.get('/search')
+async def search_detail(
+                        search_query: Search,
+                        user_agent: Optional[str] = Header(None), 
+                        x_forwarded_for: Optional[str] = Header(None),
+                       ):
+    # If the user has allowed ip access: fetch the data else: use ip geolocation
+    if search_query.lat and search_query.lon:
+        response = await es.search(
+        index = f"items-{search_query.country_code}-{search_query.language_code}",
+        body = {
+            "query": {
+            "multi_match": {
+                "query": search_query.q,
+                "type": "best_fields",
+                "fields": ["title", "description"],
+                }
+            }
+            },
+        size=20,
+        )
+        return response
+    else:
+        # TODO: fetch the key fron env variables in production
+        # key = os.environ.get('IP_GEOLOCATION_KEY')
+        key = "a390f3d3aa104eb0b008f3ff8982c055"
+        r = requests.get(f"https://api.ipgeolocation.io/ipgeo?apiKey={key}&ip={x_forwarded_for}")
+        ip_response = r.json()
+        latitude = ip_response['latitude']
+        longitude = ip_response['longitude']
+        search = await es.search(
+            index = f"items-{search_query.country_code}-{search_query.language_code}",
+            body = {
+                "query": {
+                    "multi_match": {
+                        "query": search_query.query,
+                        "type": "best_fields",
+                        "fields": ["title", "description"],
+                    }
+                }
+            },
+            size=20,
+        )
+        return search
 
 @app.on_event("shutdown")
 async def app_shutdown():
     await es.close()
+
+# Boosting
+# A boost value of between 1 and 15 is recommended
+# {
+#  "query": {
+#  "bool": {
+#  "should": [
+#  { "match": {
+#  "title": {
+#  "query": "War and Peace",
+#  "boost": 2
+#  }}},
+#  { "match": {
+#  "author": {
+#  "query": "Leo Tolstoy",
+#  "boost": 2
+#  }}},
+#  { "bool": {
+#  "should": [
+#  { "match": { "translator": "Constance Garnett" }},
+#  { "match": { "translator": "Louise Maude" }}
+#  ]
+#  }}
+#  ]
+#  }
+#  }
+# }
+
+# # Disjunction Max Query
+# # simply means return documents that match any of these queries, and return the score of
+# # the best matching query
+#  {
+#  "query": {
+#  "dis_max": {
+#  "queries": [
+#  { "match": { "title": "Brown fox" }},
+#  { "match": { "body": "Brown fox" }}
+#  ]
+#  }
+#  }
+# }
+
+# # Recommended tie breaker is 0 to 1 but closest to 0
+#  "query": {
+#  "dis_max": {
+#  "queries": [
+#  { "match": { "title": "Quick pets" }},
+#  { "match": { "body": "Quick pets" }}
+#  ],
+#  "tie_breaker": 0.3
+#  }
+#  }
+# }
+
+# # Using multi_match
+# {
+#  "dis_max": {
+#  "queries": [
+#  {
+#  "match": {
+#  "title": {
+#  "query": "Quick brown fox",
+#  "minimum_should_match": "30%"
+#  }
+#  }
+#  },
+#  {
+#  "match": {
+#  "body": {
+#  "query": "Quick brown fox",
+#  "minimum_should_match": "30%"
+#  }
+#  }
+#  },
+#  ],
+#  "tie_breaker": 0.3
+#  }
+# }
+
+# # The above query can simply be written as 
+# {
+#  "multi_match": {
+#  "query": "Quick brown fox",
+#  "type": "best_fields",
+#  "fields": [ "title", "body" ],
+#  "tie_breaker": 0.3,
+#  "minimum_should_match": "30%"
+#  }
+# }
+
+# # Boosting individual Fields in multi_match queries
+# {
+#  "multi_match": {
+#  "query": "Quick brown fox",
+#  "fields": [ "*_title", "chapter_title^2" ]
+#  }
+# }
+
+# # Search-as-you-type
+# {
+#     "query": {
+#         "match_phrase_prefix": {
+#             "title": {
+#                 "query": query
+#             }
+#         }
+#     }
+# }
