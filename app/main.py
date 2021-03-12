2193564db59342a57c3b20213a8742359c96f39c
Hezekiah <38362531+Hezeh@users.noreply.github.com>
2021-03-12 14:23:12 +0300

    Removed unused code

--- app/main.py
+++ app/main.py
@@ -2,217 +2,23 @@ import os
 
 from fastapi import FastAPI, Header, Request, Response, status
 from elasticsearch import AsyncElasticsearch
-from typing import List, Optional
+from typing import Optional
 import requests
 from pydantic import BaseModel
 import json
-from geojson import Point
 import base64
-from collections import defaultdict
 
 app = FastAPI()
-
-
-class Suggestions(BaseModel):
-    q: str
-    country_code: Optional[str]
-    language_code: Optional[str]
-
-
-class Search(BaseModel):
-    q: str
-    country_code: Optional[str]
-    lat: Optional[int]
-    lon: Optional[int]
-    country_code: str
-    language_code: str
-
-
-class Location(BaseModel):
-    lat: int
-    lon: int
-
-
-class Item(BaseModel):
-    itemId: str
-    userId: str
-    images: Optional[List[str]]
-    title: Optional[str]
-    description: Optional[str]
-    price: Optional[int]
-    category: Optional[str]
-    subCategory: Optional[str]
-    location: Optional[Point]
-    locationDescription: Optional[str]
-    businessName: Optional[str]
-    businessDescription: Optional[str]
-    phoneNumber: Optional[str]
-    inStock: Optional[bool]
-    mondayOpeningHours: Optional[str]
-    mondayClosingHours: Optional[str]
-    tuesdayOpeningHours: Optional[str]
-    tuesdayClosingHours: Optional[str]
-    wednesdayOpeningHours: Optional[str]
-    wednesdayClosingHours: Optional[str]
-    thursdayOpeningHours: Optional[str]
-    thursdayClosingHours: Optional[str]
-    fridayOpeningHours: Optional[str]
-    fridayClosingHours: Optional[str]
-    saturdayOpeningHours: Optional[str]
-    saturdayClosingHours: Optional[str]
-    sundayOpeningHours: Optional[str]
-    sundayClosingHours: Optional[str]
-    businessProfilePhoto: Optional[str]
-    isMondayOpen: Optional[bool]
-    isTuesdayOpen: Optional[bool]
-    isWednesdayOpen: Optional[bool]
-    isThursdayOpen: Optional[bool]
-    isFridayOpen: Optional[bool]
-    isSaturdayOpen: Optional[bool]
-    isSundayOpen: Optional[bool]
-
-
 es = AsyncElasticsearch()
-
 
 @app.get("/")
 async def home_page():
     return {"Message": "Welcome"}
 
-
 @app.get("/ping")
 async def ping():
     resp = await es.ping()
     return resp
-
-
-@app.get("/suggestions")
-async def search_suggestions(q: str):
-    suggestions = []
-    search = await es.search(
-        index=f"items",
-        body={
-            "_source": False,
-            "suggest": {
-                "text": f"{q}",
-                "title_suggestion": {
-                    "completion": {"field": "suggest", "fuzzy": {"fuzziness": 2}}
-                },
-                "title": {"term": {"field": "title"}},
-                "title_search_as_you_type": {
-                    "term": {"field": "title.search_as_you_type"}
-                },
-                "title_2gram_search_as_you_type": {
-                    "term": {"field": "title.search_as_you_type._2gram"}
-                },
-                "title_3gram_search_as_you_type": {
-                    "term": {"field": "title.search_as_you_type._3gram"}
-                },
-                "title_term_suggest": {"term": {"field": "title.term"}},
-                "title_trigram_suggest": {
-                    "phrase": {
-                        "field": "title.trigram",
-                        "size": 1,
-                        "direct_generator": [
-                            {"field": "title.trigram", "suggest_mode": "always"},
-                            {
-                                "field": "title.reverse",
-                                "suggest_mode": "always",
-                                "pre_filter": "reverse",
-                                "post_filter": "reverse",
-                            },
-                        ],
-                    }
-                },
-                "description": {"term": {"field": "description"}},
-                "description_search_as_you_type": {
-                    "term": {"field": "description.search_as_you_type"}
-                },
-                "description_2gram_search_as_you_type": {
-                    "term": {"field": "description.search_as_you_type._2gram"}
-                },
-                "description_3gram_search_as_you_type": {
-                    "term": {"field": "description.search_as_you_type._3gram"}
-                },
-                "description_term_suggest": {"term": {"field": "description.term"}},
-                "description_trigram_suggest": {
-                    "phrase": {
-                        "field": "description.trigram",
-                        "size": 1,
-                        "direct_generator": [
-                            {"field": "title.trigram", "suggest_mode": "always"},
-                            {
-                                "field": "title.reverse",
-                                "suggest_mode": "always",
-                                "pre_filter": "reverse",
-                                "post_filter": "reverse",
-                            },
-                        ],
-                    }
-                },
-            },
-        },
-    )
-    json_suggestions_dump = json.dumps(search)
-    json_suggestions = json.loads(json_suggestions_dump)
-    all_suggestions = []
-    description_suggestions = json_suggestions["suggest"]["description"]
-    description_twogram = json_suggestions["suggest"][
-        "description_2gram_search_as_you_type"
-    ]
-    description_threegram = json_suggestions["suggest"][
-        "description_3gram_search_as_you_type"
-    ]
-    description_search_as_you_type = json_suggestions["suggest"][
-        "description_search_as_you_type"
-    ]
-    description_term = json_suggestions["suggest"]["description_term_suggest"]
-    description_trigram = json_suggestions["suggest"]["description_trigram_suggest"]
-    title = json_suggestions["suggest"]["title"]
-    title_threegram = json_suggestions["suggest"]["title_3gram_search_as_you_type"]
-    title_twogram = json_suggestions["suggest"]["title_2gram_search_as_you_type"]
-    title_search_as_you_type = json_suggestions["suggest"]["title_search_as_you_type"]
-    title_suggestion = json_suggestions["suggest"]["title_suggestion"]
-    title_term = json_suggestions["suggest"]["title_term_suggest"]
-    title_trigram = json_suggestions["suggest"]["title_trigram_suggest"]
-
-    if description_suggestions != None:
-        all_suggestions.append(description_suggestions)
-    if description_twogram != None:
-        all_suggestions.append(description_twogram)
-    if description_threegram != None:
-        all_suggestions.append(description_threegram)
-    if description_search_as_you_type != None:
-        all_suggestions.append(description_search_as_you_type)
-    if description_term != None:
-        all_suggestions.append(description_term)
-    if description_trigram != None:
-        all_suggestions.append(description_trigram)
-    if title != None:
-        all_suggestions.append(title)
-    if title_threegram != None:
-        all_suggestions.append(title_threegram)
-    if title_twogram != None:
-        all_suggestions.append(title_twogram)
-    if title_search_as_you_type != None:
-        all_suggestions.append(title_search_as_you_type)
-    if title_suggestion != None:
-        all_suggestions.append(title_suggestion)
-    if title_term != None:
-        all_suggestions.append(title_term)
-    if title_trigram != None:
-        all_suggestions.append(title_trigram)
-
-    for suggestion in all_suggestions:
-        for i in suggestion:
-            options = i["options"]
-            if options != None:
-                for option in options:
-                    text = option["text"]
-                    suggestions.append(text)
-    suggestions = list(dict.fromkeys(suggestions))
-    return {"suggestions": suggestions}
-
 
 @app.get("/search")
 async def search_detail(
@@ -222,22 +28,18 @@ async def search_detail(
     user_agent: Optional[str] = Header(None),
     x_forwarded_for: Optional[str] = Header(None),
 ):
-    # If the user has allowed location access: fetch the data else: use ip geolocation
-    # TODO: Incorporate click-through rate, item dwell-time, number of recent searches/popularity
-    # TODO: Incorporate brand
-    # TODO: Query segmentation
-    # TODO: Query Understanding
-    # TODO: Knowledge Graph
-    # TODO: Taxonomy
-    # TODO: Synonyms
-    # TODO: bool; boost term matches & and improve ranking criteria based on category & subCategory
-    # TODO: Acronym
-    # TODO: Fuzzy Matching
-    # TODO: Category recommendation
-    # TODO: Matching Criteria & Ranking Criteria
     parsed_results = []
-    if lat and lon:
-        results = await es.search(
+    latitude = lat
+    longitude = lon  
+    if latitude == None and longitude == None:
+        key = "a390f3d3aa104eb0b008f3ff8982c055"
+        r = requests.get(
+            f"https://api.ipgeolocation.io/ipgeo?apiKey={key}&ip={x_forwarded_for}"
+        )
+        ip_response = r.json()
+        latitude = ip_response["latitude"]
+        longitude = ip_response["longitude"]
+    results = await es.search(
             index="items",
             body={
                 "track_scores": True,
@@ -261,72 +63,6 @@ async def search_detail(
                                 "weight": 2.1,
                                 "gauss": {
                                     "location": {
-                                        "origin": {"lat": lat, "lon": lon},
-                                        "offset": "0km",
-                                        "scale": "4km"
-                                    }
-                                },
-                            }
-                        ],
-                    }
-                },
-                "sort": [
-                    {
-                        "_geo_distance": {
-                            "location": {"lat": lat, "lon": lon},
-                            "order": "asc",
-                            "unit": "km",
-                            "ignore_unmapped": True
-                        }
-                    }
-                ],
-                "aggs": {"map_zoom": {"geo_bounds": {"field": "location"}}},
-            },
-            size=20,
-        )
-        hits = results["hits"]["hits"]
-        if len(hits) != 0:
-            for result in hits:
-                source = result["_source"]
-                sort = result["sort"][0]
-                source["distance"] = sort
-                parsed_results.append(source)
-            bounds = results["aggregations"]["map_zoom"]["bounds"]
-            return {
-                "items": parsed_results,
-                "bounds": bounds
-            }
-        else:
-            return {}
-    else:
-        # TODO: fetch the key fron env variables in production
-        # key = os.environ.get('IP_GEOLOCATION_KEY')
-        key = "a390f3d3aa104eb0b008f3ff8982c055"
-        r = requests.get(
-            f"https://api.ipgeolocation.io/ipgeo?apiKey={key}&ip={x_forwarded_for}"
-        )
-        ip_response = r.json()
-        latitude = ip_response["latitude"]
-        longitude = ip_response["longitude"]
-        results = await es.search(
-            index=f"items",
-            body={
-                "track_scores": True,
-                "query": {
-                    "function_score": {
-                        "score_mode": "sum",
-                        "query": {
-                            "multi_match": {
-                                "query": q,
-                                "fields": ["title^10", "description"],
-                                "tie_breaker": 0.3
-                            }
-                        },
-                        "functions": [
-                            {
-                                "weight": 2.1,
-                                "gauss": {
-                                    "location": {
                                         "origin": {"lat": latitude, "lon": longitude},
                                         "offset": "0km",
                                         "scale": "4km"
@@ -339,7 +75,7 @@ async def search_detail(
                 "sort": [
                     {
                         "_geo_distance": {
-                            "location": {"lat": lat, "lon": longitude},
+                            "location": {"lat": latitude, "lon": longitude},
                             "order": "asc",
                             "unit": "km",
                             "ignore_unmapped": True
@@ -350,7 +86,9 @@ async def search_detail(
             },
             size=20,
         )
-        for result in results["hits"]["hits"]:
+    hits = results["hits"]["hits"]
+    if len(hits) != 0:
+        for result in hits:
             source = result["_source"]
             sort = result["sort"][0]
             source["distance"] = sort
@@ -358,12 +96,14 @@ async def search_detail(
         bounds = results["aggregations"]["map_zoom"]["bounds"]
         return {
             "items": parsed_results,
-            "bounds": bounds,
+            "bounds": bounds
         }
-
+    else:
+        return {}
 
 async def item_index(id, body):
     resp = await es.index(index="items", id=id, body=body)
+    return resp
 
 
 @app.post("/index")
@@ -397,12 +137,6 @@ async def update_index(request: Request):
     json_payload["location"] = {"lat": latitude, "lon": longitude}
     resp = await item_update(id, json_payload)
     return resp
-
-
-class DeleteModel(BaseModel):
-    itemId: str
-    admin_email: str
-
 
 async def item_delete(id):
     # TODO: If doc exists then delete else just return
@@ -472,13 +206,11 @@ async def recs(lat: Optional[float] = None, lon: Optional[float] = None):
     buckets = resp["aggregations"]["categories"]["buckets"]
     for bucket in buckets:
         category = bucket["key"]
-        # recs = defaultdict(list)
         recs = []
         if len(docs) != 0:
             for doc in docs:
                 doc_category = doc["_source"]["category"]
                 if doc_category == category:
-                    # recs[f"{category}"].append(doc["_source"])
                     recs.append(doc["_source"])
         recs_list.append({"category": f"{category}", "items": recs})
     return {"recommendations": recs_list}
@@ -504,11 +236,6 @@ async def item_viewstream(request: Request, response: Response):
 async def item_viewstream_index(id, body):
     resp = await es.index(index="item-viewstream", id=id, body=body)
     return resp
-
-
-# @app.post('/merchant-viewstream')
-# async def item
-
 
 @app.on_event("shutdown")
 async def app_shutdown():
