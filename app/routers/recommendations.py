a37d10aa594b4bf9d2bb55a6dc72876a706b6e80
Hezekiah <38362531+Hezeh@users.noreply.github.com>
2021-07-03 11:08:46 +0300

    New func for fetching recs

--- app/routers/recommendations.py
+++ app/routers/recommendations.py
@@ -3,6 +3,7 @@ from typing import Optional
 import uuid
 import requests
 from .app_variables import es
+from .recs_categories import recs_categories
 
 router = APIRouter()
 
@@ -95,6 +96,114 @@ async def recs(lat: Optional[float] = None, lon: Optional[float] = None, x_forwa
                 if doc_category == category:
                     recs.append(doc["_source"])
         recs_list.append({"category": f"{category}", "items": recs})
+    return {
+        "recsId": recsId,
+        "recommendations": recs_list
+    }
+
+@router.get("/test-recs", tags=["recommendations"])
+async def test_recs(lat: Optional[float] = None, lon: Optional[float] = None, x_forwarded_for: Optional[str] = Header(None),):
+    latitude = lat
+    longitude = lon
+    recsId = uuid.uuid4() 
+    if latitude == None and longitude == None:
+        key = "a390f3d3aa104eb0b008f3ff8982c055"
+        r = requests.get(
+            f"https://api.ipgeolocation.io/ipgeo?apiKey={key}&ip={x_forwarded_for}"
+        )
+        if r.status_code == 200:
+            ip_response = r.json()
+            latitude = ip_response["latitude"]
+            longitude = ip_response["longitude"]
+        else:
+            latitude = 0
+            longitude = 0
+    resp = await es.search(
+        index="items",
+        body={
+            "function_score": {
+            "score_mode": "sum",
+            "functions": [
+                {
+                    "weight": 1.2,
+                    "filter": {
+                        "bool": {
+                        "should": [
+                            {
+                            "term": {
+                                "inStock": True
+                            }
+                            }
+                        ]
+                        }
+                    }
+                },
+                {
+                    "weight": 2.1,
+                    "gauss": {
+                        "location": {
+                            "origin": {
+                                "lat": 0.5,
+                                "lon": 5
+                            },
+                            "offset": "0km",
+                            "scale": "4km"
+                        }
+                    }
+                }
+            ]
+            },
+            "sort": [
+                {
+                    "_geo_distance": {
+                        "location": {
+                            "lat": 0.6,
+                            "lon": 5
+                        },
+                        "order": "asc",
+                        "unit": "km",
+                        "ignore_unmapped": True
+                    }
+                }
+            ]
+        },
+    )
+    docs = resp["hits"]["hits"]
+    recs_list = []
+    # Get all the different categories
+    categories = []
+    if len(docs) != 0:
+        for doc in docs:
+            # Extract the catory of the document
+            doc_category = doc["_source"]["category"]
+            if (doc_category not in categories):
+                # Add the category to the categories list
+                categories.append(doc_category)
+    for category in categories:
+        recs = []
+        if len(docs) != 0:
+            for doc in docs:
+                doc_category = doc["_source"]["category"]
+                if doc_category == category:
+                    recs.append(doc["_source"])
+        recs_list.append(
+            {
+                "category": f"{category}"
+            },
+            {
+                "items": recs
+            }
+        )
+    # buckets = resp["aggregations"]["categories"]["buckets"]
+    # for bucket in buckets:
+    #     category = bucket["key"]
+    #     recs = []
+    #     if len(docs) != 0:
+    #         for doc in docs:
+    #             doc_category = doc["_source"]["category"]
+    #             if doc_category == category:
+    #                 recs.append(doc["_source"])
+    #     recs_list.append({"category": f"{category}", "items": recs})
     return {
         "recsId": recsId,
         "recommendations": recs_list
