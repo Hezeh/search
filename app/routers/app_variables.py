b84f8c5d2fca0e224681d5ff8b1e0897b3207bb2
Hezekiah <38362531+Hezeh@users.noreply.github.com>
2021-08-04 16:02:03 +0300

    Use service account

--- app/routers/app_variables.py
+++ app/routers/app_variables.py
@@ -5,16 +5,16 @@ import firebase_admin
 
 es = AsyncElasticsearch()
 
-project_id = 'beammart'
-# Use the application default credentials
-cred = credentials.ApplicationDefault()
-firebase_admin.initialize_app(cred, {
-  'projectId': project_id,
-})
+# project_id = 'beammart'
+# # Use the application default credentials
+# cred = credentials.ApplicationDefault()
+# firebase_admin.initialize_app(cred, {
+#   'projectId': project_id,
+# })
 
 # Use a service account
-# cred = credentials.Certificate('/home/hezekiah/firestore-beammart.json')
-# firebase_admin.initialize_app(cred)
+cred = credentials.Certificate('/home/hezekiah/firestore-beammart.json')
+firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 transaction = db.transaction()
