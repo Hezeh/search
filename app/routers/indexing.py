ce9c7abffe1efd37a4f2ff18a3053ab7cd9974fa
Hezekiah <38362531+Hezeh@users.noreply.github.com>
2021-08-12 12:57:03 +0300

    Added Services Indexing logic

--- app/routers/indexing.py
+++ app/routers/indexing.py
@@ -9,7 +9,6 @@ async def item_index(id, body):
     resp = await es.index(index="items", id=id, body=body)
     return resp
 
-
 @router.post("/index", tags=["indexing"])
 async def index_es(request: Request):
     envelope = await request.body()
@@ -24,6 +23,46 @@ async def index_es(request: Request):
     resp = await item_index(id, json_payload)
     return resp
 
+async def services_index(id, body):
+    resp = await es.index(index="services", id=id, body=body)
+    return resp
+
+@router.post("/service-create", tags=["indexing"])
+async def service_index_es(request: Request):
+    envelope = await request.body()
+    pubsub_message = json.loads(envelope.decode("utf-8"))
+    payload = base64.b64decode(pubsub_message["message"]["data"])
+    json_payload = json.loads(payload)
+    id = json_payload["serviceId"]
+    if "location" in json_payload:
+        latitude = json_payload["location"]["_latitude"]
+        longitude = json_payload["location"]["_longitude"]
+        json_payload["location"] = {"lat": latitude, "lon": longitude}
+    resp = await services_index(id, json_payload)
+    return resp
+
+async def service_update(id, body):
+    resp = await es.update(index="services", id=id, body={"doc": body})
+    return resp
+
+
+@router.post("/service-update", tags=["indexing"])
+async def update_service_index(request: Request):
+    envelope = await request.body()
+    pubsub_message = json.loads(envelope.decode("utf-8"))
+    payload = base64.b64decode(pubsub_message["message"]["data"])
+    json_payload = json.loads(payload)
+    id = json_payload["serviceId"]
+    latitude = json_payload["location"]["_latitude"]
+    longitude = json_payload["location"]["_longitude"]
+    json_payload["location"] = {"lat": latitude, "lon": longitude}
+    exists = await es.exists(index="services", id=id)
+    if exists:
+        resp = await service_update(id, json_payload)
+        return resp
+    else:
+        resp = await services_index(id, json_payload)
+        return resp
 
 async def item_update(id, body):
     resp = await es.update(index="items", id=id, body={"doc": body})
@@ -64,6 +103,27 @@ async def delete_document(request: Request):
     exists = await es.exists(index="items", id=id)
     if exists:
         resp = await item_delete(id)
+        return resp
+    else:
+        return {}
+
+
+async def service_delete(id):
+    # TODO: If doc exists then delete else just return
+    resp = await es.delete(index="services", id=id)
+    return resp
+
+
+@router.post("/service-delete", tags=["indexing"])
+async def delete_service_document(request: Request):
+    envelope = await request.body()
+    pubsub_message = json.loads(envelope.decode("utf-8"))
+    payload = base64.b64decode(pubsub_message["message"]["data"])
+    json_payload = json.loads(payload)
+    id = json_payload["serviceId"]
+    exists = await es.exists(index="services", id=id)
+    if exists:
+        resp = await service_delete(id)
         return resp
     else:
         return {}
