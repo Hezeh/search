cda4b92daa98627bfd69614f981de10409ed0d90
Hezekiah <38362531+Hezeh@users.noreply.github.com>
2021-08-13 13:40:48 +0300

    Services recs

--- app/routers/recommendations.py
+++ app/routers/recommendations.py
@@ -214,3 +214,91 @@ async def recs(lat: Optional[float] = None, lon: Optional[float] = None, x_forwa
         "recsId": recsId,
         "recommendations": recs_list
     }
+
+
+@router.get("/services-recs", tags=["recommendations"])
+async def recs(lat: Optional[float] = None, lon: Optional[float] = None, x_forwarded_for: Optional[str] = Header(None),):
+    latitude = lat
+    longitude = lon
+    recsId = uuid.uuid4() 
+    if latitude == None and longitude == None:
+        key = "a390f3d3aa104eb0b008f3ff8982c055"
+        r = requests.get(
+            f"https://api.ipgeolocation.io/ipgeo?apiKey={key}&ip={x_forwarded_for}"
+        )
+        if r.status_code == 200:
+            ip_response = r.json()
+            latitude = ip_response["latitude"]
+            longitude = ip_response["longitude"]
+        else:
+            latitude = 0
+            longitude = 0
+    resp = await es.search(
+        size=50,
+        index="services",
+        body={
+            "track_scores": True,
+            "query": {
+                "function_score": {
+                "score_mode": "sum",
+                "functions": [
+                    {
+                    "weight": 2.1,
+                    "gauss": {
+                        "location": {
+                        "origin": {
+                            "lat": latitude,
+                            "lon": longitude,
+                        },
+                        "offset": "0km",
+                        "scale": "4km"
+                        }
+                    }
+                    }
+                ]
+                }
+            },
+            "sort": [
+                {
+                "_geo_distance": {
+                    "location": {
+                    "lat": latitude,
+                    "lon": longitude,
+                    },
+                    "order": "asc",
+                    "unit": "km",
+                    "ignore_unmapped": True
+                }
+                }
+            ]
+            } 
+    )
+    docs = resp["hits"]["hits"]
+    recs_list = []
+    # Get all the different categories
+    categories = []
+    if len(docs) != 0:
+        for doc in docs:
+            # Extract the category of the document
+            doc_category = doc["_source"]["businessServiceCategory"]
+            if (doc_category not in categories):
+                # Add the category to the categories list
+                categories.append(doc_category)
+    for category in categories:
+        recs = []
+        if len(docs) != 0:
+            for doc in docs:
+                doc_category = doc["_source"]["businessServiceCategory"]
+                if doc_category == category:
+                    recs.append(doc["_source"])
+        recs_list.append(
+            {
+                "category": f"{category}",
+                "services": recs
+            }
+        )
+    return {
+        "recsId": recsId,
+        "recommendations": recs_list
+    }
+
--- app/routers/search.py
+++ app/routers/search.py
@@ -120,3 +120,119 @@ async def search_detail(
         }
     else:
         return {}
+
+
+@router.get("/services", tags=["search"])
+async def search_service(
+    q: str,
+    lat: Optional[float] = None,
+    lon: Optional[float] = None,
+    user_agent: Optional[str] = Header(None),
+    x_forwarded_for: Optional[str] = Header(None),
+):
+    parsed_results = []
+    latitude = lat
+    longitude = lon
+    searchId = uuid.uuid4()  
+    if latitude == None and longitude == None:
+        key = "a390f3d3aa104eb0b008f3ff8982c055"
+        r = requests.get(
+            f"https://api.ipgeolocation.io/ipgeo?apiKey={key}&ip={x_forwarded_for}"
+        )
+        ip_response = r.json()
+        latitude = ip_response["latitude"]
+        longitude = ip_response["longitude"]
+    results = await es.search(
+            index="services",
+            body={
+                "track_scores": True,
+                "query": {
+                    "function_score": {
+                        "score_mode": "sum",
+                        "query": {
+                            "multi_match": {
+                                "query": q,
+                                "fields": [
+                                    "serviceName^20",
+                                    "serviceDescription^15",
+                                    "category^5",
+                                    "subCategory^18",
+                                    "businessName^5",
+                                    "locationDescription^10",
+                                    "businessDescription^5"
+                                ],
+                                "tie_breaker": 0.3,
+                                "fuzziness": "AUTO"
+                            }
+                        },
+                        "functions": [
+                            {
+                                "weight": 4,
+                                "filter": {
+                                    "bool": {
+                                        "should": [
+                                            {
+                                                "term": {
+                                                    "promoted": True
+                                                }
+                                            }
+                                        ]
+                                    }
+                                }
+                                },
+                                {
+                                "weight": 1.2,
+                                "filter": {
+                                    "bool": {
+                                        "should": [
+                                            {
+                                                "term": {
+                                                    "inStock": True
+                                                }
+                                            }
+                                        ]
+                                    }
+                                }
+                            },
+                            {
+                                "weight": 2.1,
+                                "gauss": {
+                                    "location": {
+                                        "origin": {"lat": latitude, "lon": longitude},
+                                        "offset": "0km",
+                                        "scale": "4km"
+                                    }
+                                }
+                            }
+                        ]
+                    }
+                },
+                "sort": [
+                    {
+                        "_geo_distance": {
+                            "location": {"lat": latitude, "lon": longitude},
+                            "order": "asc",
+                            "unit": "km",
+                            "ignore_unmapped": True
+                        }
+                    }
+                ],
+                "aggs": {"map_zoom": {"geo_bounds": {"field": "location"}}}
+            },
+            size=20
+        )
+    hits = results["hits"]["hits"]
+    if len(hits) != 0:
+        for result in hits:
+            source = result["_source"]
+            sort = result["sort"][0]
+            source["distance"] = sort
+            parsed_results.append(source)
+        bounds = results["aggregations"]["map_zoom"]["bounds"]
+        return {
+            "services": parsed_results,
+            "bounds": bounds,
+            "searchId": searchId
+        }
+    else:
+        return {}
