24f8f566672c95b8b7b1ba7b297b5a8ba8ce054b
Hezekiah <38362531+Hezeh@users.noreply.github.com>
2021-08-23 14:04:15 +0300

    Added new params to get rider

--- app/routers/logistics.py
+++ app/routers/logistics.py
@@ -6,6 +6,12 @@ import json
 
 router = APIRouter()
 
+class DeliveryDetails(BaseModel):
+  item_name: Optional[str]
+  amount: Optional[str]
+  pickup_date: Optional[str]
+  note: Optional[str]
+
 class Recipient(BaseModel):
     recipient_name: Optional[str]
     recipient_phone: Optional[str]
@@ -36,7 +42,8 @@ class MerchantAddress(BaseModel):
 async def get_rider(recipient: Recipient, 
                     merchant: Merchant, 
                     customer_address: CustomerAddress, 
-                    merchant_address: MerchantAddress
+                    merchant_address: MerchantAddress,
+                    order_details: DeliveryDetails,
                 ):
 
     sendy_api = "https://apitest.sendyit.com/v1/##request"
@@ -75,15 +82,15 @@ async def get_rider(recipient: Recipient,
         "sender_notify": merchant.sender_notify
       },
       "delivery_details": {
-        "pick_up_date": "2021-06-25 12:12:12",
+        "pick_up_date": order_details.pickup_date,
         "collect_payment": {
           "status": True,
           "pay_method": 0,
-          "amount": 10
+          "amount": order_details.amount,
         },
         "carrier_type": 1,
         "return": False,
-        "note": " Sample note",
+        "note": "",
         "note_status": True,
         "request_type": "delivery",
         "order_type": "ondemand_delivery",
@@ -96,7 +103,7 @@ async def get_rider(recipient: Recipient,
             "height": 10,
             "width": 200,
             "length": 30,
-            "item_name": "laptop"
+            "item_name": order_details.item_name
           }
         ]
       }
@@ -110,77 +117,3 @@ async def get_rider(recipient: Recipient,
     r = requests.post(sendy_api, data = json_data, headers=headers)
     print(r.status_code)
     return r.json()
-
-
-# @router.post("/sendy", tags=["Sendy"])
-# async def sendy_request():
-#     sendy_api = "https://apitest.sendyit.com/v1/##request"
-#     values = {
-#     "command": "request",
-#     "data": {
-#       "api_key": "H42bgpNvPPChZ8Yhh8NO",
-#       "api_username": "beammartcorporation",
-#       "vendor_type": 1,
-#       "rider_phone":"0728561783",
-#       "from": {
-#         "from_name": "Westlands",
-#         "from_lat": -1.2681032,
-#         "from_long": 36.8012739,
-#         "from_description": ""
-#       },
-#       "to": {
-#         "to_name": "Mamlaka",
-#         "to_lat": -1.2891308,
-#         "to_long": 36.8187401,
-#         "to_description": ""
-#       },
-#       "recepient": {
-#         "recepient_name": "Hezekiah",
-#         "recepient_phone": "0709779779",
-#         "recepient_email": "hezekiahmaina3@gmail.com",
-#         "recepient_notes": "recepient specific Notes",
-#         "recepient_notify": True
-#       },
-#       "sender": {
-#         "sender_name": "Beammart Corporation",
-#         "sender_phone": "0709 779 779",
-#         "sender_email": "hezekiah@beammart.app",
-#         "sender_notes": "Sender specific notes",
-#         "sender_notify": True
-#       },
-#       "delivery_details": {
-#         "pick_up_date": "2021-06-25 12:12:12",
-#         "collect_payment": {
-#           "status": True,
-#           "pay_method": 0,
-#           "amount": 10
-#         },
-#         "carrier_type": 1,
-#         "return": False,
-#         "note": " Sample note",
-#         "note_status": True,
-#         "request_type": "delivery",
-#         "order_type": "ondemand_delivery",
-#         "ecommerce_order": False,
-#         "express": False,
-#         "skew": 1,
-#         "package_size": [
-#           {
-#             "weight": 20,
-#             "height": 10,
-#             "width": 200,
-#             "length": 30,
-#             "item_name": "laptop"
-#           }
-#         ]
-#       }
-#     },
-#     "request_token_id": "request_token_id"
-#   }
-#     headers = {
-#         'Content-Type': 'application/json'
-#     }
-#     json_data = json.dumps(values)
-#     r = requests.post(sendy_api, data = json_data, headers=headers)
-#     print(r.status_code)
-#     return r.json()
